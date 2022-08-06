b0023245d02448c792c47347911397ddcfbeaa02
vero <veronica.delacruz12@hotmail.com>
2022-08-06 17:41:11 -0500

    Feat: Create api to connect the front-end with back-end

---
+++ src/Api/travelApi.jsx
@@ -0,0 +1,32 @@
+const API_URL = "http://localhost:3000/todos";
+
+export async function getTodos() {
+  try {
+    const response = await fetch(API_URL);
+    return response.json();
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function deleteTodo(id) {
+  try {
+    const response = await fetch(${API_URL}/delete/${id}, {
+      method: "PATCH",
+    });
+    return response.json();
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function completeTodo(id) {
+  try {
+    const response = await fetch(${API_URL}/complete/${id}, {
+      method: "PATCH",
+    });
+    return response.json();
+  } catch (error) {
+    console.log(error);
+  }
+}
